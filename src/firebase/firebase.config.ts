9fc713c0260a21c9e8246eff72ac33f7c62cad1f
Koino Engineering <email@koino.engineering>
2020-04-01 00:29:54 +0900

    #1 add firebase config

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1N0g7_OgXd8HzQZ_1HCjxYKj6H0qwx0Q",
+    authDomain: "chatmarubatsu.firebaseapp.com",
+    databaseURL: "https://chatmarubatsu.firebaseio.com",
+    projectId: "chatmarubatsu",
+    storageBucket: "chatmarubatsu.appspot.com",
+    messagingSenderId: "912501704929",
+    appId: "1:912501704929:web:b0cb1779fb83fc1a780fa0",
+    measurementId: "G-94T60ZCLBK"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebaseApp.firestore();
