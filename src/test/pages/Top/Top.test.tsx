eb0b9e2cdef8f9af49a286c43e384d3eef0d90a6
KoinoEngineering <email@koino.engineering>
2020-04-03 09:54:00 +0900

    #1 App -> Top

--- src/test/pages/Top/Top.test.tsx
+++ src/test/pages/Top/Top.test.tsx
@@ -1,9 +1,9 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import App from "pages/App/App";
+import Top from "pages/Top/Top";
 
 test("renders learn react link", () => {
-    const { getByText } = render(<App />);
+    const { getByText } = render(<Top />);
     const linkElement = getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
 });
